c24a04de8f9594a63e0f962fc2cf5144103f695d
Martin Štekl <martin.stekl@gmail.com>
2012-07-21 15:15:32 +0200

    + vytvoření základu ukázkové aplikace ze sandboxu Nette

---
+++ app/presenters/HomepagePresenter.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Homepage presenter.
+ *
+ * @author     John Doe
+ * @package    MyApplication
+ */
+class HomepagePresenter extends BasePresenter {
+
+	public function renderDefault() {
+	}
+
+}
